a16b0be578cc3297d43e2bd514c006dad153d5f2
jackyk <jackykimani13@gmail.com>
2016-04-27 18:29:35 +0300

    Becomes and addict

---
+++ day3/man_importer.py
@@ -0,0 +1,8 @@
+from test import super_sum, hello_again, max_min, other_people
+
+a=[233,56,7,8,9,3]
+
+#print hello_again("Joey",34)
+print super_sum(*a)
+print max_min(a)
+print hello_again(*other_people[1])
---
+++ day3/test.py
@@ -0,0 +1,23 @@
+other_people=[
+    ("Joey",78),
+    ("Jik",78),
+    ("Joyt",78)
+    ]
+def super_sum(*args):
+    return sum(args)
+
+def hello_again(name,age):
+    return "I am {} and {} years old".format(name, age)
+
+def max_min(A):
+    '''Returns the max value - min value'''
+
+    max_ , min_ = A[0], A[0]
+
+    for i in A:
+        if i > max_:
+            max_ = i
+        elif i < min_:
+            min_= i
+
+    return max_ - min_
